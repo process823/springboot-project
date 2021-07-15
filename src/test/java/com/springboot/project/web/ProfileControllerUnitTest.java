48a425ba7217e8f05114e48afdd6d52d4877eb6f
태인 <process0823@gmail.com>
2021-07-15 20:10:12 +0900

    무중단배포 Nginx 테스트 코드 추가

---
+++ src/test/java/com/springboot/project/web/ProfileControllerUnitTest.java
@@ -0,0 +1,35 @@
+package com.springboot.project.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_default가_조회된다() {
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
